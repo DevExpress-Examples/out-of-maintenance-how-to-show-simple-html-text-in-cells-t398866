8348594045b5ba0eb3f5d0c3f2bd6b5fd70fc89d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SimpleHtmlEdit/Main.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Repository;
+
+namespace SimpleHtmlEditExample {
+    public partial class Main : XtraForm {
+        public Main() {
+            InitializeComponent();
+
+            RepositoryItemHypertextLabel repositoryItem = new RepositoryItemHypertextLabel();
+            gridControl1.RepositoryItems.Add(repositoryItem);
+            gridControl1.DataSource = SimpleDataSource.GetTestData();
+            gridView1.Columns[0].ColumnEdit = repositoryItem;
+        }
+    }
+    public class SimpleDataSource {
+        public string HtmlText { get; set; }
+        public string PlainText { get; set; }
+
+        SimpleDataSource(string htmlText, string plainText) {
+            HtmlText = htmlText;
+            PlainText = plainText;
+        }
+
+        public static BindingList<SimpleDataSource> GetTestData() {
+            BindingList<SimpleDataSource> testDataList = new BindingList<SimpleDataSource>();
+            testDataList.Add(new SimpleDataSource("One <b>word</b> is bold and <href=https://www.devexpress.com/Support/Center/Example/Details/T398866>another two</href> are link", "text"));
+            testDataList.Add(new SimpleDataSource("<s>Strikethrough</s> word, <i>three italic words</i>", "text"));
+            testDataList.Add(new SimpleDataSource("First <href=https://www.devexpress.com/Support/Center/Example/Details/T398866>link</href>, second <href=https://www.devexpress.com/Support/Center/Example/Details/T398866>link</href>", "text"));
+
+            return testDataList;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,21 @@
+# How to show simple HTML text in cells
+
+
+This sample illustrates how to implement a custom editor to visualize simple HTML. You can use HTML tags (e.g. <b>, <i>, <u>, <s>, <href>) to make your text more informative and interactive.<br><br><img src="https://raw.githubusercontent.com/DevExpress-Examples/how-to-show-simple-html-text-in-cells-t398866/17.2.3+/media/6e0b8bb0-41b3-11e6-80bf-00155d62480c.png"><br><br>The editor was inherited from TextEdit, which already allows drawing simple HTML strings. But, this capability is disabled in TextEdit. To implement it  in our custom editor, override TextEdit's <strong>AllowHtmlString</strong> property to return "true" all the time.<br>
+
+
+```cs
+public override bool AllowHtmlString { get { return true; } }
+```
+
+
+So now, our SimpleHtmlEdit can draw HTML strings. To make it more interactive, a few lines of code were added to handle the cursor's changing capability and open links in a browser.<br><br>See also:<br><a href="https://documentation.devexpress.com/#windowsforms/CustomDocument4874">HTML Text Formatting</a><br>General information about <a href="https://documentation.devexpress.com/#WindowsForms/CustomDocument4716">custom editors</a>
+
+
+<h3>Description</h3>
+
+<p>Starting from version 17.2, a new&nbsp;<strong>RepositoryItemHypertextLabel</strong>&nbsp;was added. This repository item should be used when it is necessary to show read-only HTML formatted text.&nbsp;</p>
+
+<br/>
+
+
